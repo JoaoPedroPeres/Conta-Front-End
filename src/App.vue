14bfc691afb6310c6256072ffafd09fc28674fbb
João Pedro <jonyecx@gmail.com>
2019-08-08 22:15:53 -0300

    Parte Cidades Do Front End Criada

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <div>
+      <v-toolbar class=primary>
+        <v-toolbar-title>Sua Conta</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+          <router-link to="/">
+            <v-btn class="link" text>Home</v-btn>
+          </router-link>
+          <router-link to="/cidades">
+            <v-btn class="link" text>Cidades</v-btn>
+          </router-link>
+          <router-link to="/agencias">
+            <v-btn class="link" text>Agências</v-btn>
+          </router-link>
+          <router-link to="/clientes">
+            <v-btn class="link" text>Clientes</v-btn>
+          </router-link>
+        </v-toolbar-items>
+      </v-toolbar>
     </div>
-    <router-view/>
-  </div>
+    <router-view></router-view>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  .link{
+    margin-top: 14px;
+    color:red;
+  }
 </style>
+
